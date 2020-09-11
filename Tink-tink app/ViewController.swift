220b41f03bd510edb9439012b0b82b59dffc5db7
Leonteva Anastasia <lagarnas@gmail.com>
2020-09-11 09:53:47 +0400

    Initial Commit

---
+++ Tink-tink app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tink-tink app
+//
+//  Created by Анастасия Леонтьева on 11.09.2020.
+//  Copyright © 2020 lagarnas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
